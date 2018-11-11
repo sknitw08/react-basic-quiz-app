4dcd7f7b65e4871211e19b91bb4fc6f4b5b9cb75
Sanjay Kumar <sank@microsoft.com>
2018-11-11 15:30:24 +0530

    Basic Quiz App Using React

---
+++ src/AuthorQuiz.js
@@ -0,0 +1,76 @@
+import React from 'react';
+//import logo from './logo.svg';
+import './App.css';
+import './bootstrap.min.css';
+
+function Hero(props) {
+  return(
+    <div className = "row">
+      <div className = "jumbotron col-10 offset-1">
+        <h1> Author Quiz </h1>
+          <p>Select the book written by the author shown</p>
+      </div>
+    </div>
+  );
+}
+
+function Book({title, onClick}) {
+  return(
+    <div className = "answer" onClick= {() => {onClick(title);}}>
+      <h4> {title} </h4>
+    </div>
+  );
+}
+
+function Turn({author, books, highlight, onAnswerSelected}) {
+  function highlightToBgColor(highlight) {
+    const mapping = {
+      'none' : '',
+      'correct' : 'green',
+      'wrong' : 'red'
+    }
+    return mapping[highlight];
+  }
+  return(
+    <div className = "row turn" style={{backgroundColor: highlightToBgColor(highlight)}}>
+      <div className = "col-4 offset-1">
+        <img src={author.imageUrl} className="authorimage" alt="Author"/>
+      </div>
+      <div className = "col-6 offset-1">
+        {books.map((title)=><Book title ={title} key={title} onClick={onAnswerSelected} />)}
+      </div>
+    </div>
+  );
+}
+
+function Continue(props) {
+  return(
+    <div className = "row">
+    </div>
+  );
+}
+
+function Footer(props) {
+  return(
+    <div id="footer" className = "row">
+      <div className="col-12">
+        <p className="text-muted credit">
+          All images are from <a href="http://commons.wikimedia.org/wiki"> WikiMedia Commons </a> and are in the public domain
+        </p>
+      </div>
+    </div>
+  );
+}
+
+function AuthorQuiz({turnData, highlight, onAnswerSelected}) {
+  return (
+    <div className="container-fluid">
+      <Hero />
+      <Turn {...turnData} highlight={highlight} onAnswerSelected={onAnswerSelected}/>
+      <Continue />
+      <Footer />
+    </div>
+  );
+}
+
+export default AuthorQuiz;
